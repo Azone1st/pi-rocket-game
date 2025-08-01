5d8cab1f405130ceff19f5beef60693b47e75067
Azone1st <thietkexaydung.monero@gmail.com>
2025-08-01 10:11:04 +0700

    server.js

--- server.js
+++ server.js
@@ -75,9 +75,11 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, () => {
-  console.log(`Pi Rocket Server running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
+
   console.log('Pi Rocket Server running on http://localhost:3000');
   startGame();
 });
